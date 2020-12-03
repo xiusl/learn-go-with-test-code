718d02a2c7d4f9425bc0bf19b27233eada7f8c32
xiusl <xiushilin@hotmail.com>
2020-12-03 14:19:55 +0800

    refactor hello

--- hello/hello.go
+++ hello/hello.go
@@ -1,13 +1,15 @@
 package main
 
+const spanish = "Spanish"
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
 
 func Hello(name string, language string) string {
     if name == "" {
         name = "World"
     }
-    if language == "Spanish" {
-        return "Hola, " + name
+    if language == spanish {
+        return spanishHelloPrefix + name
     }
     return englishHelloPrefix + name
 }
