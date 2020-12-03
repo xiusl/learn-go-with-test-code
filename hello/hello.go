1ace6a226e7bb58f4b186f177417caed758a942b
xiusl <xiushilin@hotmail.com>
2020-12-03 14:01:45 +0800

    add default

--- hello/hello.go
+++ hello/hello.go
@@ -3,7 +3,11 @@ package main
 import "fmt"
 
 const englishHelloPrefix = "Hello, "
+
 func Hello(name string) string {
+    if name == "" {
+        name = "World"
+    }
     return englishHelloPrefix + name
 }
 
