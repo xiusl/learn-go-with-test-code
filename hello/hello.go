dd20766ddd34771ce14a05a2ebe616caeab443fb
xiusl <xiushilin@hotmail.com>
2020-12-03 14:11:55 +0800

    add Spanish

--- hello/hello.go
+++ hello/hello.go
@@ -1,16 +1,14 @@
 package main
 
-import "fmt"
-
 const englishHelloPrefix = "Hello, "
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
     if name == "" {
         name = "World"
+    }
+    if language == "Spanish" {
+        return "Hola, " + name
     }
     return englishHelloPrefix + name
 }
 
-func main() {
-    fmt.Println(Hello(""))
-}
