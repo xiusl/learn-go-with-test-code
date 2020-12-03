092f38ab77e3c7df008a435030af0b97e8434a4e
xiusl <xiushilin@hotmail.com>
2020-12-03 15:19:18 +0800

    new test

--- integers/addr_test.go
+++ integers/addr_test.go
@@ -1,6 +1,7 @@
 package integers
 
 import "testing"
+import "fmt"
 
 func TestAdder(t *testing.T) {
     sum := Add(2, 2)
@@ -9,4 +10,10 @@ func TestAdder(t *testing.T) {
     if sum != expected {
         t.Errorf("expected '%d' but got '%d'", expected, sum)
     }
+}
+
+func ExampleAdd() {
+    sum := Add(1, 5)
+    fmt.Println(sum)
+    // Output: 6
 }
