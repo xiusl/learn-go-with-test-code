a694cd7bca0761735cfceb9dc9b2e0accce0aee5
xiusl <xiushilin@hotmail.com>
2020-12-03 14:52:48 +0800

    fix test & add a simple func Add()

--- integers/addr_test.go
+++ integers/addr_test.go
@@ -6,7 +6,7 @@ func TestAdder(t *testing.T) {
     sum := Add(2, 2)
     expected := 4
 
-    if sum == expected {
+    if sum != expected {
         t.Errorf("expected '%d' but got '%d'", expected, sum)
     }
 }
