e153a5a14875b12229053b9bf257d4b5b9e127b6
xiusl <xiushilin@hotmail.com>
2020-12-08 16:34:15 +0800

    add arr test

---
+++ arr/arr_test.go
@@ -0,0 +1,15 @@
+package arr
+
+import "testing"
+
+func testSum(t *testing.T) {
+
+    numbers := [5]int{1, 2, 3, 4, 5}
+
+    got := Sum(numbers)
+    want := 15
+
+    if got != want {
+        t.Errorf("got %d want %d", got, want)
+    }
+}
