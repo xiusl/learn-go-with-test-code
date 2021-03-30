40a4968c4b05c47cf33dca1ac1929323d69c2bb6
xiusl <xiushilin@hotmail.com>
2021-03-30 17:53:34 +0800

    add note

--- 08-injection/greet.go
+++ 08-injection/greet.go
@@ -13,3 +13,18 @@ func Greet(writer io.Writer, name string) {
 func main() {
 	Greet(os.Stdout, "Jack")
 }
+
+/*NOTE
+package fmt
+func Fprintf(w io.Writer, format string, a ...interface{}) (n int, err error) {
+	//...
+}
+
+package io
+type Writer interface {
+    Write(p []byte) (n int, err error)
+}
+io.Writer 是一个很好的通用接口，用于「将数据放在某个地方」
+
+fmt.Fprintf 允许传入一个 io.Writer 接口，我们知道 os.Stdout 和 bytes.Buffer 都实现了它
+*/
