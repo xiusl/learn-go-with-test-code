3a372ff4c69fe012e584e68e23ec0847f511010d
xiusl <xiushilin@hotmail.com>
2021-03-30 10:07:40 +0800

    slice: sum all

--- slice/Sum.go
+++ slice/Sum.go
@@ -1,6 +1,6 @@
 package slice
 
-func Sum(numbers [5]int) (sum int) {
+func Sum(numbers []int) (sum int) {
 	sum = 0
 
 	//for i := 0; i < 5; i++ {
@@ -10,6 +10,16 @@ func Sum(numbers [5]int) (sum int) {
 	// 使用 range
 	for _, number := range numbers {
 		sum += number
+	}
+	return
+}
+
+func SumAll(numbersToSum ...[]int) (sums []int) {
+	lengthOfNumbers := len(numbersToSum)
+	sums = make([]int, lengthOfNumbers)
+
+	for i, numbers := range numbersToSum {
+		sums[i] = Sum(numbers)
 	}
 	return
 }
--- slice/sum_test.go
+++ slice/sum_test.go
@@ -1,19 +1,22 @@
 package slice
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 
-	t.Run("collection of 5 numbers", func(t *testing.T){
-		numbers := [5]int{1, 2, 3, 4, 5}
-
-		got := Sum(numbers)
-		want := 15
-
-		if want != got {
-			t.Errorf("got %d want %d given, %v", got, want, numbers)
-		}
-	})
+	//t.Run("collection of 5 numbers", func(t *testing.T){
+	//	numbers := [5]int{1, 2, 3, 4, 5}
+	//
+	//	got := Sum(numbers)
+	//	want := 15
+	//
+	//	if want != got {
+	//		t.Errorf("got %d want %d given, %v", got, want, numbers)
+	//	}
+	//})
 
 	t.Run("collection of any size", func(t *testing.T) {
 		numbers := []int{1, 2, 3}
@@ -25,3 +28,22 @@ func TestSum(t *testing.T) {
 	})
 }
 
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{1, 2}, []int{0, 8})
+	want := []int{3, 8}
+
+	// slice can only be compared to nil
+	//if got != want {
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
+
+/*
+	reflect.DeepEqual() 不是类型安全的，下面的代码会通过编译，使用时要注意
+	a := "abc"
+	b := []int{1, 2}
+	if reflect.DeepEqual(a, b) {
+		fmt.Println("equal")
+	}
+*/
