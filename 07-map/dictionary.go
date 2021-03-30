3fd27da46254ae248ee9b8987d7664c9160dd081
xiusl <xiushilin@hotmail.com>
2021-03-30 14:22:58 +0800

    refactor

--- 07-map/dictionary.go
+++ 07-map/dictionary.go
@@ -1,5 +1,7 @@
 package _7_map
 
-func Search(dictionary map[string]string, key string) string {
+type Dictionary map[string]string
+
+func Search(dictionary Dictionary, key string) string {
 	return dictionary[key]
 }
--- 07-map/dictionary_test.go
+++ 07-map/dictionary_test.go
@@ -3,11 +3,17 @@ package _7_map
 import "testing"
 
 func TestSearch(t *testing.T) {
-	dictionary := map[string]string{"test": "one one one"}
+	dictionary := Dictionary{"test": "one one one"}
 
 	got := Search(dictionary, "test")
 	want := "one one one"
+	assertString(t, got, want)
+}
+
+func assertString(t *testing.T, got, want string) {
+	t.Helper()
+
 	if got != want {
-		t.Errorf("got '%s' want '%s' given '%v'", got, want, dictionary)
+		t.Errorf("got '%s' want '%s'", got, want)
 	}
 }
