c491ded5a9c1cead9650d06ff2f748c85467134c
xiusl <xiushilin@hotmail.com>
2021-03-30 09:50:18 +0800

    use range

--- slice/Sum.go
+++ slice/Sum.go
@@ -2,8 +2,14 @@ package slice
 
 func Sum(numbers [5]int) (sum int) {
 	sum = 0
-	for i := 0; i < 5; i++ {
-		sum += numbers[i]
+
+	//for i := 0; i < 5; i++ {
+	//	sum += numbers[i]
+	//}
+
+	// 使用 range
+	for _, number := range numbers {
+		sum += number
 	}
 	return
 }
