ae32dd0a521724929bc913d0d8ed9c3335b2b32e
xiusl <xiushilin@hotmail.com>
2021-03-30 10:37:22 +0800

    rectangle struct

--- struct/shapes.go
+++ struct/shapes.go
@@ -1,5 +1,21 @@
 package _struct
 
+type Rectangle struct {
+	 Width 	float64
+	 Height float64
+}
+
+func Perimeter(rect Rectangle) float64 {
+	return 2 * (rect.Width + rect.Height)
+}
+
+func Area(rect Rectangle) float64 {
+	return rect.Width * rect.Height
+}
+
+
+/* Version 1
+
 func Perimeter(width, height float64) float64 {
 	return 2 * (width + height)
 }
@@ -7,3 +23,4 @@ func Perimeter(width, height float64) float64 {
 func Area(width, height float64) float64 {
 	return width * height
 }
+ */
--- struct/shapes_test.go
+++ struct/shapes_test.go
@@ -2,23 +2,28 @@ package _struct
 
 import "testing"
 
-func TestPerimeter(t *testing.T) {
-	got := Perimeter(10.0, 10.0)
-	want := 40.0
-
+func checkResult(t *testing.T, got, want float64) {
 	if got != want {
 		t.Errorf("got %.2f want %.2f", got, want)
 	}
 }
 
 
+func TestPerimeter(t *testing.T) {
+	rect := Rectangle{10, 10}
+	got := Perimeter(rect)
+	want := 40.0
+
+	checkResult(t, got, want)
+}
+
+
 func TestArea(t *testing.T) {
-	got := Area(6, 7)
+	rect := Rectangle{6, 7}
+	got := Area(rect)
 	want := 42.0
 
-	if got != want {
-		t.Errorf("got %.2f want %.2f", got, want)
-	}
+	checkResult(t, got, want)
 }
 
 /*NOTE
