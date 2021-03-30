a059a2967dc976cc72a43e0b7469fc1362a61e88
xiusl <xiushilin@hotmail.com>
2021-03-30 11:00:33 +0800

    add circle shape

--- struct/shapes.go
+++ struct/shapes.go
@@ -1,10 +1,34 @@
 package _struct
 
+import "math"
+
 type Rectangle struct {
 	 Width 	float64
 	 Height float64
 }
 
+func (rect Rectangle)Perimeter() float64 {
+	return 2 * (rect.Width + rect.Height)
+}
+
+func (rect Rectangle)Area() float64 {
+	return rect.Width * rect.Height
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func (circle Circle)Perimeter() float64 {
+	return 2 * math.Pi * circle.Radius
+}
+
+func (circle Circle)Area() float64 {
+	return math.Pi * circle.Radius * circle.Radius
+}
+
+
+/* Version 2
 func Perimeter(rect Rectangle) float64 {
 	return 2 * (rect.Width + rect.Height)
 }
@@ -12,10 +36,9 @@ func Perimeter(rect Rectangle) float64 {
 func Area(rect Rectangle) float64 {
 	return rect.Width * rect.Height
 }
-
+*/
 
 /* Version 1
-
 func Perimeter(width, height float64) float64 {
 	return 2 * (width + height)
 }
--- struct/shapes_test.go
+++ struct/shapes_test.go
@@ -10,20 +10,41 @@ func checkResult(t *testing.T, got, want float64) {
 
 
 func TestPerimeter(t *testing.T) {
-	rect := Rectangle{10, 10}
-	got := Perimeter(rect)
-	want := 40.0
 
-	checkResult(t, got, want)
+	t.Run("Rect Perimeter", func(t *testing.T) {
+		rect := Rectangle{10, 10}
+		got := rect.Perimeter()
+		want := 40.0
+
+		checkResult(t, got, want)
+	})
+
+	t.Run("Circle Perimeter", func(t *testing.T) {
+		circle := Circle{5}
+		got := circle.Perimeter()
+		want := 31.41592653589793
+
+		checkResult(t, got, want)
+	})
 }
 
 
 func TestArea(t *testing.T) {
-	rect := Rectangle{6, 7}
-	got := Area(rect)
-	want := 42.0
+	t.Run("Rect Area", func(t *testing.T) {
+		rect := Rectangle{6, 7}
+		got := rect.Area()
+		want := 42.0
 
-	checkResult(t, got, want)
+		checkResult(t, got, want)
+	})
+
+	t.Run("Circle Area", func(t *testing.T) {
+		circle := Circle{10}
+		got := circle.Area()
+		want := 314.1592653589793
+
+		checkResult(t, got, want)
+	})
 }
 
 /*NOTE
