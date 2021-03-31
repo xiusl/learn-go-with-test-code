77e24fccee78a4a74701e6221e4b3807ec3e752f
xiusl <xiushilin@hotmail.com>
2021-03-31 16:02:57 +0800

    test pass but not good

--- 12-reflection/reflection.go
+++ 12-reflection/reflection.go
@@ -5,7 +5,7 @@ import "reflect"
 func Walk(x interface{}, fn func(input string)) {
 	val := reflect.ValueOf(x)
 	for i := 0; i < val.NumField(); i++ {
-		field := val.Field(0)
+		field := val.Field(i)
 		fn(field.String())
 	}
 }
