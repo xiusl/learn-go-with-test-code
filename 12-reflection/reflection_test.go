48fde0eb1d25f3cfbc123a6b1283f1e2b1d2a36d
xiusl <xiushilin@hotmail.com>
2021-03-31 16:00:29 +0800

    bad test

--- 12-reflection/reflection_test.go
+++ 12-reflection/reflection_test.go
@@ -1,6 +1,9 @@
 package _2_reflection
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestWalk(t *testing.T) {
 	expected := "Jack"
@@ -20,6 +23,45 @@ func TestWalk(t *testing.T) {
 
 	if got[0] != expected {
 		t.Errorf("got '%s' want '%s'", got[0], expected)
+	}
+
+}
+
+func TestWalkV2(t *testing.T) {
+
+	testCases := []struct{
+		Name string
+		Input interface{}
+		ExpectedCalls []string
+	}{
+		{
+			Name: "Struct with one string field",
+			Input: struct {
+				Name string
+			}{"Jack"},
+			ExpectedCalls: []string{"Jack"},
+		},
+		{
+			Name: "Struct with two string",
+			Input: struct {
+				Name string
+				City string
+			}{"Jack", "HZ"},
+			ExpectedCalls: []string{"Jack", "HZ"},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.Name, func(t *testing.T) {
+			var got []string
+			Walk(tc.Input, func(input string) {
+				got = append(got, input)
+			})
+
+			if !reflect.DeepEqual(got, tc.ExpectedCalls) {
+				t.Errorf("got %v want %v", got, tc.ExpectedCalls)
+			}
+		})
 	}
 
 }
