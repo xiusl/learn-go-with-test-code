ef0507a301ee47cd743145aaef2b7fef4281c002
xiusl <xiushilin@hotmail.com>
2021-04-01 14:45:16 +0800

    common test pass

--- 15-property/numeral.go
+++ 15-property/numeral.go
@@ -2,6 +2,42 @@ package _5_property
 
 import "strings"
 
+type RomanNumeral struct {
+	Value int
+	Symbol string
+}
+
+var allRomanNumerals = []RomanNumeral {
+	{1000, "M"},
+	{900, "CM"},
+	{500, "D"},
+	{400, "CD"},
+	{100, "C"},
+	{90, "XC"},
+	{50, "L"},
+	{40, "XL"},
+	{10, "X"},
+	{9, "IX"},
+	{5, "V"},
+	{4, "IV"},
+	{1, "I"},
+}
+
+
+func ConvertToRoman(num int) string {
+ 	var res strings.Builder
+
+ 	for _, numeral := range allRomanNumerals {
+ 		for num >= numeral.Value {
+ 			res.WriteString(numeral.Symbol)
+ 			num -= numeral.Value
+		}
+	}
+
+	return res.String()
+}
+
+/*
 func ConvertToRoman(num int) string {
 	var res strings.Builder
 
@@ -18,8 +54,7 @@ func ConvertToRoman(num int) string {
 	}
 	return res.String()
 }
-
-
+*/
 /*
 func ConvertToRoman(num int) string {
 	var res strings.Builder
