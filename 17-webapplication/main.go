3a8c791fde8670c14b36ca1bebd8a7954e33369b
xiusl <xiushilin@hotmail.com>
2021-04-01 18:21:46 +0800

    run the application

--- 17-webapplication/main.go
+++ 17-webapplication/main.go
@@ -5,8 +5,16 @@ import (
 	"net/http"
 )
 
+type InMemoryPlayerStore struct {
+
+}
+
+func (s *InMemoryPlayerStore)GetPlayerScore(name string) string {
+	return "120"
+}
+
 func main() {
-	server := &PlayerServer{}
+	server := &PlayerServer{&InMemoryPlayerStore{}}
 
 	if err := http.ListenAndServe(":5000", http.HandlerFunc(server.ServerHTTP)); err != nil {
 		log.Fatalf("could not listen on port 5000 %v", err)
