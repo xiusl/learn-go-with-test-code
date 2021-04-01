f0ccfcd2dcdbea6d2186f17d8b22523f762d16c7
xiusl <xiushilin@hotmail.com>
2021-04-01 18:02:54 +0800

    refactor

--- 17-webapplication/server_test.go
+++ 17-webapplication/server_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -8,30 +9,35 @@ import (
 
 func TestPlayerServer(t *testing.T) {
 	t.Run("return Score of Like", func(t *testing.T) {
-		request, _ := http.NewRequest(http.MethodGet, "/players/Like", nil)
+		request := newGetScoreRequest("Like")
 		recorder := httptest.NewRecorder()
 
 		PlayerServer(recorder, request)
 
-		got := recorder.Body.String()
-		want := "20"
 
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertResponseBody(t, recorder.Body.String(), "20")
 	})
 
 	t.Run("return Score of Jack", func(t *testing.T) {
-		request, _ := http.NewRequest(http.MethodGet, "/players/Jack", nil)
+		request := newGetScoreRequest("Jack")
 		recorder := httptest.NewRecorder()
 
 		PlayerServer(recorder, request)
 
-		got := recorder.Body.String()
-		want := "30"
-
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertResponseBody(t, recorder.Body.String(), "30")
 	})
+}
+
+func newGetScoreRequest(name string) *http.Request {
+	url := fmt.Sprintf("/players/%s", name)
+	request, _ := http.NewRequest(http.MethodGet, url, nil)
+	return request
+}
+
+
+func assertResponseBody(t *testing.T, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
 }
