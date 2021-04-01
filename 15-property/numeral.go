e6266baaca36b96d597f2d83e8f6d7ffa7a4559c
xiusl <xiushilin@hotmail.com>
2021-04-01 14:29:50 +0800

    test pass

--- 15-property/numeral.go
+++ 15-property/numeral.go
@@ -5,8 +5,28 @@ import "strings"
 func ConvertToRoman(num int) string {
 	var res strings.Builder
 
+	for i := num; i > 0; i-- {
+		if i == 5 {
+			res.WriteString("V")
+			break
+		}
+		if i == 4 {
+			res.WriteString("IV")
+			break
+		}
+		res.WriteString("I")
+	}
+	return res.String()
+}
+
+
+/*
+func ConvertToRoman(num int) string {
+	var res strings.Builder
+
 	for i := 0; i < num; i++ {
 		res.WriteString("I")
 	}
 	return res.String()
 }
+*/
