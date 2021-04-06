b5ac5c329e49002dae76e8add2dc913310ad3c6c
xiusl <xiushilin@hotmail.com>
2021-04-06 13:50:21 +0800

    issue: Cannot assign value to unexported field 'playerStore'

--- 21-cli/cmd/cli/main.go
+++ 21-cli/cmd/cli/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	poker "github.com/xiusl/go-learn/21-cli"
+	"log"
+	"os"
+)
+
+const dbFileName = "cli.game.json"
 
 func main() {
-	fmt.Println("Let`s go")
+	db, err := os.OpenFile(dbFileName, os.O_RDWR|os.O_CREATE, 0666)
+
+	if err != nil {
+		log.Fatalf("problem opening file %s %v", dbFileName, err)
+	}
+
+	store, err := poker.NewFileSystemStore(db)
+
+	if err != nil {
+		log.Fatalf("problem creating file system player store %v", err)
+	}
+
+	game := poker.CLI{store, os.Stdin}
+	game.PlayPoker()
 }
