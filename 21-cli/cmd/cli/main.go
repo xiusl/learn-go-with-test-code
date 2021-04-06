bce50f59248259bd348c67b9a59e93b6cdb285c1
xiusl <xiushilin@hotmail.com>
2021-04-06 13:52:16 +0800

    fix CLI main

--- 21-cli/cmd/cli/main.go
+++ 21-cli/cmd/cli/main.go
@@ -21,6 +21,6 @@ func main() {
 		log.Fatalf("problem creating file system player store %v", err)
 	}
 
-	game := poker.CLI{store, os.Stdin}
+	game := poker.NewCLI(store, os.Stdin)
 	game.PlayPoker()
 }
