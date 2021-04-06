262debd18c4329d1539da03b87e8e97c5a745310
xiusl <xiushilin@hotmail.com>
2021-04-06 15:38:38 +0800

    refactor

--- 22-time/CLI.go
+++ 22-time/CLI.go
@@ -6,44 +6,38 @@ import (
 	"io"
 	"strconv"
 	"strings"
-	"time"
 )
 
 const PlayerPrompt = "Please enter the number of players: "
 
 type CLI struct {
-	playerStore PlayerStore
 	in  *bufio.Scanner
 	out io.Writer
-	alerter BlindAlerter
+	g *Game
 }
 
 func NewCLI(store PlayerStore, in io.Reader, out io.Writer, alerter BlindAlerter) *CLI {
-	return &CLI{store, bufio.NewScanner(in), out, alerter}
+	return &CLI{bufio.NewScanner(in), out, &Game{
+		alerter,
+		store,
+	}}
 }
 
 func (cli *CLI) PlayPoker() {
+	// 提示用户输入玩家数量
 	_, _  = fmt.Fprint(cli.out, PlayerPrompt)
 
 	// 读取用户输入，并转换为 int 类型，忽略错误
-	numberOfPlayers, _ := strconv.Atoi(cli.readLine())
+	numberOfPlayersInput := cli.readLine()
+	numberOfPlayers, _ := strconv.Atoi(strings.Trim(numberOfPlayersInput, "\n"))
 
-	cli.scheduleBlindAlerts(numberOfPlayers)
-	userInput := cli.readLine()
-	cli.playerStore.RecordWin(extractWinner(userInput))
-}
+	//
+	cli.g.Start(numberOfPlayers)
 
-func (cli *CLI)scheduleBlindAlerts(numOfPlayers int) {
-	// base 5 minute
-	// 1 minute per player
-	blindIncrement := time.Duration(5 + numOfPlayers) * time.Minute
+	winnerInput := cli.readLine()
+	winner := extractWinner(winnerInput)
 
-	blinds := []int{100, 200, 300, 400, 500, 600, 800, 1000, 2000, 4000, 8000}
-	blindTime := 0 * time.Second
-	for _, blind := range blinds {
-		cli.alerter.ScheduleAlertAt(blindTime, blind)
-		blindTime = blindTime + blindIncrement
-	}
+	cli.g.Finish(winner)
 }
 
 func extractWinner(userInput string) string {
