c8ec84c09af489610d457384d3b6401b1647e2d3
xiusl <xiushilin@hotmail.com>
2021-04-06 14:37:10 +0800

    make test pass

--- 22-time/CLI.go
+++ 22-time/CLI.go
@@ -4,18 +4,21 @@ import (
 	"bufio"
 	"io"
 	"strings"
+	"time"
 )
 
 type CLI struct {
 	playerStore PlayerStore
 	in  *bufio.Scanner
+	alerter BlindAlerter
 }
 
 func NewCLI(store PlayerStore, in io.Reader, alerter BlindAlerter) *CLI {
-	return &CLI{store, bufio.NewScanner(in)}
+	return &CLI{store, bufio.NewScanner(in), alerter}
 }
 
 func (cli *CLI) PlayPoker() {
+	cli.alerter.ScheduleAlertAt(5 * time.Second, 100)
 	userInput := cli.readLine()
 	cli.playerStore.RecordWin(extractWinner(userInput))
 }
